3e51222a70d9d4d139ec5f8e91cccb0ce43cf638
Lucas Marandat <lucas.mrdt@gmail.com>
2020-09-03 03:22:38 +0200

    update database

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,35 @@
 import React from "react";
+import GitHubIcon from "@material-ui/icons/GitHub";
 // @ts-ignore
 import SnackbarProvider from "react-simple-snackbar";
 
 import CardsProvider from "./CardsProvider";
+import { makeStyles } from "@material-ui/core";
 
-function App() {
+const App = () => {
+  const classes = useStyles();
+
   return (
     <SnackbarProvider>
+      <a
+        rel={"nofollow"}
+        href={"https://github.com/lucasmrdt/questce-le-quizz"}
+        className={classes.github}
+        target={"_blank"}
+      >
+        <GitHubIcon />
+      </a>
       <CardsProvider />
     </SnackbarProvider>
   );
-}
+};
+
+const useStyles = makeStyles(() => ({
+  github: {
+    position: "absolute",
+    top: 10,
+    right: 10,
+  },
+}));
 
 export default App;
