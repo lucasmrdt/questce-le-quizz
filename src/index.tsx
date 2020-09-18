a0a99513cb0aece4099f2b80e481e2f678d80166
lucasmrdt <lucasmrdt@localhost.localdomain>
2020-09-18 11:30:43 +0200

    update firebase

--- src/index.tsx
+++ src/index.tsx
@@ -8,14 +8,14 @@ import "./index.css";
 import "katex/dist/katex.min.css";
 
 firebase.initializeApp({
-  apiKey: "AIzaSyBHvpgiWbMfgp-vQlLBE5FMHTJdA88Ofac",
-  authDomain: "questce-ca378.firebaseapp.com",
-  databaseURL: "https://questce-ca378.firebaseio.com",
-  projectId: "questce-ca378",
-  storageBucket: "questce-ca378.appspot.com",
-  messagingSenderId: "984317793270",
-  appId: "1:984317793270:web:3728bda392b7d5e7b36dd7",
-  measurementId: "G-G65J1VR2EZ",
+  apiKey: "AIzaSyBEhIHkUCLmfRHzHiC0WLLofw9uL2ugOQk",
+  authDomain: "quiz-l3-info.firebaseapp.com",
+  databaseURL: "https://quiz-l3-info.firebaseio.com",
+  projectId: "quiz-l3-info",
+  storageBucket: "quiz-l3-info.appspot.com",
+  messagingSenderId: "920736359774",
+  appId: "1:920736359774:web:fabd99a3c7a5404276dbcf",
+  measurementId: "G-EK9LVBQNLE",
 });
 firebase.analytics();
 
